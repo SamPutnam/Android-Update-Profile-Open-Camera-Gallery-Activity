2d4c94099f5fabdd9f14e04453682ccaf2bbc6e9
Sam Putnam <samhputnam@gmail.com>
2016-04-06 17:37:32 -0400

    Change button given title, selection, and positive button to close dialog

--- app/src/main/java/com/samuelputnam/one/MainActivity.java
+++ app/src/main/java/com/samuelputnam/one/MainActivity.java
@@ -1,8 +1,13 @@
 package com.samuelputnam.one;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.app.AlertDialog;
+import android.widget.Button;
+import android.content.DialogInterface;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button_Change);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Pick Profile Picture");
+                alertDialog.setMessage("this is my app");
+
+                alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // here you can add functions
+                    }
+                });
+                alertDialog.show();
+            }
+    });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.overflow, menu);
         return true;
     }
+
 }
