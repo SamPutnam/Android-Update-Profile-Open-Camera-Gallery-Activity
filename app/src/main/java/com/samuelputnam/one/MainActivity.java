1881873ef760ee1671e606c93818c20d376d7b54
Sam Putnam <samhputnam@gmail.com>
2016-04-05 22:25:00 -0400

    Overflow menu that displays settings entry

--- app/src/main/java/com/samuelputnam/one/MainActivity.java
+++ app/src/main/java/com/samuelputnam/one/MainActivity.java
@@ -2,6 +2,7 @@ package com.samuelputnam.one;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.overflow, menu);
+        return true;
     }
 }
