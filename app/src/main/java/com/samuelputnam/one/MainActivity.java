e4854ea00050d491bf7d7de661d2269aa249c22d
Sam Putnam <samhputnam@gmail.com>
2016-04-07 16:07:44 -0400

    Alert Builder Dialog instantiated

--- app/src/main/java/com/samuelputnam/one/MainActivity.java
+++ app/src/main/java/com/samuelputnam/one/MainActivity.java
@@ -8,8 +8,10 @@ import android.view.View;
 import android.app.AlertDialog;
 import android.widget.Button;
 import android.content.DialogInterface;
+import android.app.Dialog;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,25 +23,38 @@ public class MainActivity extends AppCompatActivity {
 
             public void onClick(View view) {
 
-                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-                alertDialog.setTitle("Pick Profile Picture");
-                alertDialog.setMessage("this is my app");
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle(R.string.pick_profile_picture);
+                alertDialog.setItems(R.array.change_button_items);
 
-                        //bottom-right button in dialog
+                //bottom-right button in dialog
             /*    alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         // here you can add functions
                         // a comment
                     }
                 });*/
-                alertDialog.show();
+                AlertDialog dialog = builder.create();
+
             }
-    });
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.overflow, menu);
         return true;
     }
-
+/*
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.pick_profile_picture)
+                .setItems(R.array.change_button_items, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // The 'which' argument contains the index position
+                        // of the selected item
+                    }
+                });
+        return builder.create();
+    }*/
 }
+
