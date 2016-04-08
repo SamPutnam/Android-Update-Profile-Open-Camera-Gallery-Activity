9bb13173139900db960c84e30ef86086ca6190ec
Sam Putnam <samhputnam@gmail.com>
2016-04-08 12:05:48 -0400

    Android Camera app opened from dialog button press

--- app/src/main/java/com/samuelputnam/one/MainActivity.java
+++ app/src/main/java/com/samuelputnam/one/MainActivity.java
@@ -3,6 +3,7 @@ package com.samuelputnam.one;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.app.AlertDialog;
@@ -10,6 +11,7 @@ import android.widget.Button;
 import android.content.DialogInterface;
 import android.app.Dialog;
 import android.support.v4.app.Fragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,9 +29,13 @@ public class MainActivity extends AppCompatActivity {
 
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
                 alertDialog.setTitle(R.string.pick_profile_picture);
+
                 alertDialog.setItems(R.array.change_button_items, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialogInterface, int item) {
+                    public void onClick(DialogInterface dialogInterface, int which) {
                         // open the camera or the gallery
+                        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                        startActivity(intent);
+
                     }
                 });
                 AlertDialog alert = alertDialog.create();
