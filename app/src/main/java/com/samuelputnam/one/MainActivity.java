1dc0ca9ab645424909337eb970189d3da033ed4a
Sam Putnam <samhputnam@gmail.com>
2016-04-08 12:39:43 -0400

    File created to save image; image file name set

--- app/src/main/java/com/samuelputnam/one/MainActivity.java
+++ app/src/main/java/com/samuelputnam/one/MainActivity.java
@@ -11,10 +11,18 @@ import android.widget.Button;
 import android.content.DialogInterface;
 import android.app.Dialog;
 import android.support.v4.app.Fragment;
+import android.provider.MediaStore;
+import java.net.URI;
+import android.net.Uri;
+import java.io.File;
+import android.os.Environment;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int MEDIA_TYPE_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
                         // open the camera or the gallery
                         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
                         startActivity(intent);
+                        Uri fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
 
                     }
                 });
@@ -48,4 +59,26 @@ public class MainActivity extends AppCompatActivity {
             getMenuInflater().inflate(R.menu.overflow, menu);
             return true;
         }
+    private static File getOutputMediaFile(int type) {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+
+        return mediaFile;
+    }
+        /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
 }
