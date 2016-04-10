1a226606ccf21368069224dc95005f33f5737bd7
Sam Putnam <samhputnam@gmail.com>
2016-04-10 18:11:42 -0400

    Added tailored loadsnap(), createImageFile(), cropImage()  methods

--- app/src/main/java/com/samuelputnam/one/MainActivity.java
+++ app/src/main/java/com/samuelputnam/one/MainActivity.java
@@ -14,6 +14,9 @@ import android.content.DialogInterface;
 import android.app.Dialog;
 import android.support.v4.app.Fragment;
 import android.provider.MediaStore;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.URI;
 import android.net.Uri;
 import java.io.File;
@@ -23,8 +26,6 @@ import android.widget.Toast;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.io.IOException;
-
-
 
 /** help from android: http://developer.android.com/training/camera/photobasics.html */
 
@@ -37,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
     private static final int PICK_FROM_CAMERA = 1;
     private static final int PICK_FROM_FILE = 2;
     private String mCurrentPhotoPath;
+    private static final String URI_INSTANCE_STATE_KEY = "saved_uri";
+    private static final String TEMP_PHOTO_FILE = "temporary_holder.jpg";
+    private Uri mImageCaptureUri;
+    private static final String IMAGE_UNSPECIFIED = "image/*";
 
 
     /** Called when the activity is first created. */
@@ -44,9 +49,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mImageView = (ImageView) findViewById(R.id.imageView_Profile_Photo);
         mImageBitmap = null;
+        if (savedInstanceState != null) {
+            mImageCaptureUri = savedInstanceState
+                    .getParcelable(URI_INSTANCE_STATE_KEY);
+        }
+        createImageFile();
+        loadSnap();
 
 
         final Button button = (Button) findViewById(R.id.button_Change);
@@ -63,15 +73,18 @@ public class MainActivity extends AppCompatActivity {
                             // Open Camera
                             Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
                             startActivity(intent);
-                            // Specify the uri of the image
+                            //Specify the uri of the image
                             intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mCurrentPhotoPath);
+                            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
                             //startActivityForResult(intent, PICK_FROM_CAMERA);
                         }
                         if (which == 1){
                             //Select from Gallery
                             Intent intent2 = new Intent();
                             intent2.setType("image/*");
+                            intent2.putExtra("crop", "true");
                             intent2.setAction(Intent.ACTION_GET_CONTENT);
+                            intent2.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
                             startActivityForResult(Intent.createChooser(intent2, "Complete action using"), PICK_FROM_FILE);
                         }
 
@@ -87,34 +100,79 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
        if (resultCode == RESULT_OK) {
            if (requestCode == PICK_FROM_CAMERA) {
-               //doCrop();
+               cropImage();
+               Bundle extras = data.getExtras();
+               // Set the picture image in UI
+               mImageView.setImageBitmap((Bitmap) extras.getParcelable("data"));
+               File f = new File(mImageCaptureUri.getPath());
+               if (f.exists())
+                   f.delete();
            }
            if (requestCode == PICK_FROM_FILE) {
-               //doCrop();
+               cropImage();
+               Bundle extras2 = data.getExtras();
+               //mImageView.setImageBitmap((Bitmap) extras2.getParcelable("data"));
            }
        }
    }
 
+// ****************** private helper functions ***************************//
+
+    private void loadSnap() {
+        // Load profile photo from internal storage
+        try {
+            FileInputStream fis = openFileInput(mCurrentPhotoPath);
+                Bitmap bmap = BitmapFactory.decodeStream(fis);
+                mImageView.setImageBitmap(bmap);
+                fis.close();
+            }
+        catch (IOException e) {
+            // Default profile photo if no photo saved before.
+            mImageView.setImageResource(R.drawable.dartmouth);
+        }
+    }
+    private void saveSnap() {
+        // Commit all the changes into preference file
+        // Save profile image into internal storage.
+        mImageView.buildDrawingCache();
+        Bitmap bmap = mImageView.getDrawingCache();
+        try {
+            FileOutputStream fos = openFileOutput(mCurrentPhotoPath, MODE_PRIVATE);
+            bmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
 
 
+    /**
+     *
+     */
     // A method that returns a unique file name for a new photo using a date-time stamp:
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+    private File createImageFile() {
+        try {
+            // Create an image file name
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imageFileName = "JPEG_" + timeStamp + "_";
+
+            File storageDir = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES);
+            File image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+            // Save a file: path for use with ACTION_VIEW intents
+            mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        } catch (IOException e) {
+            mImageView.setImageResource(R.drawable.dartmouth);
+            e.printStackTrace();
+        }
         return image;
     }
 
-    ;
     //With the createImageFile() method available to create a file for the photo,
     // you can now create and invoke the Intent like this:
 
@@ -124,11 +182,7 @@ public class MainActivity extends AppCompatActivity {
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             // Create the File where the photo should go
             File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-            }
+            photoFile = createImageFile();
             // Continue only if the File was successfully created
             if (photoFile != null) {
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
@@ -142,7 +196,6 @@ public class MainActivity extends AppCompatActivity {
 
 		/* There isn't enough memory to open up more than a couple camera photos */
 		/* So pre-scale the target bitmap into which the file is decoded */
-
 		/* Get the size of the ImageView */
         int targetW = mImageView.getWidth();
         int targetH = mImageView.getHeight();
@@ -208,5 +261,22 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    // Crop and resize the image for profile
+    private void cropImage() {
+        // Use existing crop activity.
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(mImageCaptureUri, IMAGE_UNSPECIFIED);
+
+        // Specify image size
+        intent.putExtra("outputX", 100);
+        intent.putExtra("outputY", 100);
+
+        // Specify aspect ratio, 1:1
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("scale", true);
+        intent.putExtra("return-data", true);
+    }
 }
+
 
